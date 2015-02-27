1b2c418f97052c4fab36dee617acb07eed965a5a
Kevin Lcks <k.luckas@uni-bonn.de>
2015-02-27 23:13:00 +0100

    fuer neue kriterien

---
+++ evaluation/verdacht_neu.py
@@ -0,0 +1,34 @@
+from math import *
+import os
+fin = open('data_lehrpreis_neu.txt','r')
+fout = open('verdaechtige_lehrpreis_neu.txt', 'w')
+data = fin.readlines()
+length = len(data)
+i=0
+c=0
+d=0
+while i < length-1:
+	try: 
+		onetwo = int(data[i+3])+int(data[i+4])
+		average = float(int(data[i+3])*1 + int(data[i+4])*2 + int(data[i+5])*3 + int(data[i+6])*4 + int(data[i+7])*5 + int(data[i+8])*6)/float(data[i+9])
+		if int(data[i+9]) > 9 and float(onetwo)/float(data[i+9]) >0.7 and average<2.0:
+			pro=float(onetwo)/float(data[i+9])
+			if data[i]=='\n':
+				data = data[:i]+data[i+10:]
+				length=length - 10
+			else:
+				fout.write(data[i] + 'Vorlesung: ' + data[i+1] + 'Prozente: ' + str(pro*100)+ '\n' + 'Durchschnittsnote: ' + str(average) + '\n' + 'abgegebene Stimmen: ' + data[i+9] +'\n\n')
+				i=i+10
+				c=c+1
+				d=d+1
+		else:
+			i=i+10
+			d=d+1
+	except:
+		i=i+1
+
+fout.write('Insgesamt werden ' + str(c) + ' aus ' + str(d) + ' Vorlesungen nominiert')
+os.system('clear')
+print 'Insgesamt werden ' + str(c) + ' aus ' + str(d) + ' Vorlesungen nominiert!'
+fin.close()
+fout.close()
---
+++ evaluation/verdacht_neu.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+echo 'Lehrpreisauswertungs-Script v3beta'
+echo ' '
+echo 'Python-Script by Kevin Luckas, bash-Script by Christopher F. Joern'
+
+
+for folder in */
+        do
+		NAME=`cat $folder/lecturer_name`
+		LECTURE=`cat $folder/lecture_title`
+		a='Der Name des Dozenten ('
+		b=') des Moduls '
+		c=' wurde in die Datei kopiert!'
+		d='Die Werte des Moduls '
+		e=' gehalten von '
+		f=' wurden in die Datei kopiert!'
+		PHRASE1=$a$NAME$b$LECTURE$c	
+		PHRASE2=$d$LECTURE$e$NAME$f	
+		echo $NAME >> data_lehrpreis_neu.txt
+		echo $LECTURE >> data_lehrpreis_neu.txt
+		echo $PHRASE1
+                grep -c [0] ${folder}/q26.data >> data_lehrpreis_neu.txt
+		grep -c [1] ${folder}/q26.data >> data_lehrpreis_neu.txt
+		grep -c [2] ${folder}/q26.data >> data_lehrpreis_neu.txt
+		grep -c [3] ${folder}/q26.data >> data_lehrpreis_neu.txt
+		grep -c [4] ${folder}/q26.data >> data_lehrpreis_neu.txt
+		grep -c [5] ${folder}/q26.data >> data_lehrpreis_neu.txt
+		grep -c [6] ${folder}/q26.data >> data_lehrpreis_neu.txt
+		grep -c [0123456] ${folder}/q26.data >> data_lehrpreis_neu.txt
+                echo $PHRASE2
+        done
+
+ipython verdacht_neu.py
+echo 'Die Daten wurden ausgewertet!'
+echo 'Viel Spass!'
+x=1
+while [ $x = 1 ]
+do
+	read -p "Die Liste der nominierten Dozenten anzeigen? y|n `echo $'\n>'`" answ
+	if [ "$answ" = "y" ]
+			then
+				gedit verdaechtige_lehrpreis_neu.txt
+				x=0
+			elif [ "$answ" = "n" ]
+				then				
+					echo 'Gut... Dann halt nicht...'
+					x=0
+				else
+					echo 'Bitte mit y oder n antworten! Dumbf#§k...'
+	fi
+done
